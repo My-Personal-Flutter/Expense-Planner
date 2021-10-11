77d9f958e56c14511c29be5f04179784574f8744
Awais Mansha <awaismansha1998@gmail.com>
2021-10-11 23:26:52 +0500

    project working

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expense_planner/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -13,6 +14,26 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: "t1",
+      title: "New Shoes",
+      amount: 30.12,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Glasses",
+      amount: 10.12,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Shirts",
+      amount: 12.12,
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,27 +54,39 @@ class MyHomePage extends StatelessWidget {
               child: Text("Chart"),
             ),
           ),
-          Container(
-            width: double.infinity,
-            child: Card(
-              shadowColor: Colors.blue,
-              color: Colors.orange,
-              elevation: 4,
-              child: Text("Playground"),
-            ),
-          ),
-          Card(
-            shadowColor: Colors.purple,
-            color: Colors.pink,
-            elevation: 4,
-            child: Text(
-              "New Playground",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                elevation: 3,
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.red,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(tx.amount.toString()),
+                    ),
+                    Column(
+                      children: [
+                        Text(tx.title),
+                        Text(
+                          tx.date.toString(),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
+          )
         ],
       ),
     );
